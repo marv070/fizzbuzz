b0fe6343eee5f6b3b766a30894fa8d368fa807b2
Marvin Laucher <marv070@gmail.com>
2016-01-15 10:22:38 -0500

    trying different solutions to fizzbuzz or green co kaza

--- greene_co_kata.rb
+++ greene_co_kata.rb
@@ -1,17 +1,41 @@
 def fizz_buzz(num)
   arr = []
-  (1..num).each do |n|
-    if ((n % 3 == 0) && (n % 5 == 0))
-      arr << "FizzBuzz"
-    elsif (n % 3 == 0)
-      arr << "Fizz"
-    elsif (n % 5 == 0)
-      arr << "Buzz"
+  (1..num).each do |input_number|
+    if ((input_number % 3 == 0) && (input_number % 5 == 0))
+      arr << "GreeneCounty"
+    elsif (input_number % 3 == 0)
+      arr << "Greene"
+    elsif (input_number % 5 == 0)
+      arr << "County"
     else
-      arr << n
+      arr << input_number
     end
   end
   puts arr
 end
 
-fizz_buzz(1000)
+   fizz_buzz(100)
+
+  # 1.upto(100) do |input_number|
+  #     if input_number % 5 == 0 and input_number % 3 == 0
+  #       puts "GreeneCounty"
+  #     elsif input_number % 3 == 0
+  #       puts "Greene"
+  #     elsif input_number % 5 == 0
+  #       puts "County"
+  #     else
+  #       puts input_number
+  #     end
+  #   end
+
+  # (1..100).each do |input_number|  # solution without using array
+  #  greene = input_number % 3 == 0  
+  #  county = input_number.modulo(5) == 0  # mod symbol % or .modulo will work the same
+
+  #  puts case
+  #    when (greene and county) then 'GreeneCounty'
+  #    when greene then 'Greene'
+  #    when county then 'County'
+  #    else input_number
+  #  end
+  # end
